2c876e04347edc2dcce463063dc0e7e79f2628e5
怪兽产品 <zhangdeyang@enmonster.com>
2018-08-20 10:39:11 +0800

    over4

--- src/js/config.js
+++ src/js/config.js
@@ -2,5 +2,5 @@ module.exports = {
     // webapiDomain: 'http://vinoyy.com/',
     // webapiDomain: 'http://106.12.35.219:3000/'
     // webapiDomain: 'http://localhost:3000/'
-    webapiDomain: 'http://danone.missxiaolin.com/'
+    webapiDomain: 'http://api.vinoyy.com/'
 }
