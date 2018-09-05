22df3f3dbc77a09badb8969c254595200c912ef3
怪兽产品 <zhangdeyang@enmonster.com>
2018-09-05 14:30:48 +0800

    测试号做不了支付demo……gg

--- src/views/payDemo.vue
+++ src/views/payDemo.vue
@@ -23,8 +23,11 @@ export default {
                 package: "", // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
                 signType: "", // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
                 paySign: "", // 支付签名
-                success: function(res) {
+                success: res => {
                     // 支付成功后的回调函数
+                },
+                cancel: res => {
+                    // 取消支付后的回调函数
                 }
             });
         }
